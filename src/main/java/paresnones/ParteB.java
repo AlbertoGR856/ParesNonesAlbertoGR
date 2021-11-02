aec2c09dc936e9c22b3a4510ac979aecc83a5df6
AlbertoGR <albertogr578@gmail.com>
2021-11-02 13:25:51 +0100

    Añade la selección de dedos de los jugadores, usando la clase random para el jugador2(maquina)

--- src/main/java/paresnones/ParteB.java
+++ src/main/java/paresnones/ParteB.java
@@ -16,9 +16,11 @@ public class ParteB {
     public static void main(String[] args) {
 
         Random aleatorio = new Random();
-        
+
         int decisionJugador1;
         int decisionJugador2;
+        int dedosJugador1;
+        int dedosJugador2;
 
         JOptionPane.showMessageDialog(null, "BIENVENIDO A PARES Y NONES");
         JOptionPane.showMessageDialog(null, "Seleccione Pares o Nones (1- Pares, 0- Nones)");
@@ -33,16 +35,32 @@ public class ParteB {
 
         } while (decisionJugador1 < 0 || decisionJugador1 > 1);
 
+        JOptionPane.showMessageDialog(null, "Se generara la decision del jugador2(maquina)");
         do {
 
-            decisionJugador2 = aleatorio.nextInt(0) + 1;
-
-            JOptionPane.showMessageDialog(null, "La respuesta del jugador 1 es de: " + decisionJugador2);
+            decisionJugador2 = aleatorio.nextInt(1) + 1;
 
         } while (decisionJugador2 < 0 || decisionJugador2 > 1 || decisionJugador2 == decisionJugador1);
 
+        do {
+
+            String texto3;
+
+            texto3 = JOptionPane.showInputDialog("Introduzca los dedos del jugador1:");
+
+            dedosJugador1 = Integer.parseInt(texto3);
+
+        } while (dedosJugador1 < 0 || dedosJugador1 > 10);
+
+        do {
+
+            dedosJugador2 = aleatorio.nextInt(10) + 1;
+
+        } while (dedosJugador2 < 0 || dedosJugador2 > 10);
         
         
+        
+
     }
 
 }
