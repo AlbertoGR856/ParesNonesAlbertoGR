f3276bee0906ef3cd76b5f8831bdf2254e9361bf
AlbertoGR <albertogr578@gmail.com>
2021-11-02 13:19:55 +0100

    Añade la decision de los jugadores utilizando la clase random con el jugador2(maquina)

--- src/main/java/paresnones/ParteB.java
+++ src/main/java/paresnones/ParteB.java
@@ -4,10 +4,45 @@
  */
 package paresnones;
 
+import java.util.Random;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author albertogarzon
  */
 public class ParteB {
-    
+
+    public static void main(String[] args) {
+
+        Random aleatorio = new Random();
+        
+        int decisionJugador1;
+        int decisionJugador2;
+
+        JOptionPane.showMessageDialog(null, "BIENVENIDO A PARES Y NONES");
+        JOptionPane.showMessageDialog(null, "Seleccione Pares o Nones (1- Pares, 0- Nones)");
+
+        do {
+
+            String texto;
+
+            texto = JOptionPane.showInputDialog("Introduzca la decision del jugador1:");
+
+            decisionJugador1 = Integer.parseInt(texto);
+
+        } while (decisionJugador1 < 0 || decisionJugador1 > 1);
+
+        do {
+
+            decisionJugador2 = aleatorio.nextInt(0) + 1;
+
+            JOptionPane.showMessageDialog(null, "La respuesta del jugador 1 es de: " + decisionJugador2);
+
+        } while (decisionJugador2 < 0 || decisionJugador2 > 1 || decisionJugador2 == decisionJugador1);
+
+        
+        
+    }
+
 }
