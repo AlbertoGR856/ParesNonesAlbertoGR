c902d590970ef515d5034325cc0f44f3072c3049
AlbertoGR <albertogr578@gmail.com>
2021-11-05 14:00:01 +0100

    Añade bucles y manejo de excepciones

--- src/main/java/paresnones/ParteB.java
+++ src/main/java/paresnones/ParteB.java
@@ -4,6 +4,7 @@
  */
 package paresnones;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,7 +15,7 @@ import java.util.Scanner;
 /*
 URL del repositorio remoto
 https://github.com/AlbertoGR856/ParesNonesAlbertoGR.git
-*/
+ */
 public class ParteB {
 
     public static void main(String[] args) {
@@ -28,8 +29,8 @@ public class ParteB {
         final int NUMERO_DEDOS_MAXIMO = 10;
 
         //Variables introducidas por teclado
-        int decisionJugador1;
-        int dedosJugador1;
+        int decisionJugador1 = 0;
+        int dedosJugador1 = 0;
 
         //Variables generadas de forma aleatoria usando la clase Random
         int decisionJugador2;
@@ -41,10 +42,15 @@ public class ParteB {
         //Variable String utilizada para repetir la ejecución del programa
         String respuesta;
 
+        //Variable booleana utilizada en los bucles que contienen el try-catch
+        boolean seguir = true;
+
         //Bucle do-while para repetir la ejecucion del programa segun la respuesta dada por el usuario
         do {
 
             System.out.println("BIENVENIDO A PARES Y NONES");
+            
+            System.out.println("-------------------------------");
 
             //SELECCIÓN PARES O NONES
             System.out.println("Seleccione Pares o Nones (1- Pares, 0- Nones)");
@@ -52,9 +58,24 @@ public class ParteB {
             //Bucle do-while para repetir la entrada de datos si no esta en el rango establecido
             do {
 
-                System.out.println("Introduzca la decision del jugador1: ");
+                //Bucle try-catch
+                do {
 
-                decisionJugador1 = teclado.nextInt();
+                    try {
+
+                        System.out.println("Introduzca la decision del jugador1: ");
+
+                        decisionJugador1 = teclado.nextInt();
+
+                        seguir = false;
+
+                    } catch (InputMismatchException ime) {
+
+                        System.out.println("El formato del numero no es valido, vuelva a introducir un numero");
+                        teclado.nextLine();
+                    }
+
+                } while (seguir);
 
                 if (decisionJugador1 < 0 || decisionJugador1 > 1) {
 
@@ -65,7 +86,6 @@ public class ParteB {
             } while (decisionJugador1 < 0 || decisionJugador1 > 1);
 
             System.out.println("Se generara la decision del jugador2(maquina)");
-            
 
             //Bucle do-while para repetir la entrada de datos si no esta en el rango establecido
             do {
@@ -76,18 +96,38 @@ public class ParteB {
                 //Se incluye en la condicion del while que la respues del jugador2 sea igual a la del jugador1. Ya que ambos no pueden elegir lo mismo, por lo tanto
                 //si es falso finalizara el bucle
             } while (decisionJugador2 < 0 || decisionJugador2 > 1 || decisionJugador2 == decisionJugador1);
-            
+
             System.out.println("El jugador2(maquina) ha elegido: " + decisionJugador2);
 
+            //Actualizacion de la variable booleana
+            seguir = true;
+
+            System.out.println("-------------------------------");
+            
             //SELECCIÓN DEDOS DE LOS JUGADORES
             System.out.println("Seleccione los dedos para ambos jugadores (Minimo 0 y maximo 10)");
 
             //Bucle do-while para repetir la entrada de datos si no esta en el rango establecido
             do {
 
-                System.out.println("Introduzca los dedos del jugador1: ");
+                //Bucle try-catch
+                do {
 
-                dedosJugador1 = teclado.nextInt();
+                    try {
+
+                        System.out.println("Introduzca los dedos del jugador1: ");
+
+                        dedosJugador1 = teclado.nextInt();
+
+                        seguir = false;
+
+                    } catch (InputMismatchException ime) {
+                        System.out.println("El formato del numero no es valido, vuelva a introducir un numero");
+                        teclado.nextLine();
+
+                    }
+
+                } while (seguir);
 
                 if (dedosJugador1 < NUMERO_DEDOS_MINIMO || dedosJugador1 > NUMERO_DEDOS_MAXIMO) {
 
@@ -110,6 +150,8 @@ public class ParteB {
             sumaDedosJugadores = dedosJugador1 + dedosJugador2;
 
             System.out.println("Los dedos del jugador2(maquina) son: " + dedosJugador2);
+
+            System.out.println("-------------------------------");
             
             //RESULTADO
             System.out.println("La suma de los dedos de ambos jugadores es de: " + sumaDedosJugadores);
@@ -151,6 +193,8 @@ public class ParteB {
 
             }
 
+            System.out.println("-------------------------------");
+            
             //Al finalizar la estructura if-else se pide por consola si el usuario quiere realizar otra iteraccion
             System.out.println("¿Quieres volver a jugar?");
             System.out.println("S o cualquier tecla - Si");
